32e511063dd29da88abe6c7b9d6d70c915f8878b
Nick06888 <75800411+Nick06888@users.noreply.github.com>
2022-05-26 14:50:13 +0200

    Update Program.cs

--- source-code-EXPLAINED/Program.cs
+++ source-code-EXPLAINED/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;//declared for Thread.Sleep(1000); (line 42)
+using System.Threading;//declared for Thread.Sleep(1000); (line 43)
 
 namespace Indietro
 {
